7de6dd27712b9991b2b0d4706a34201c28ae07ff
faide <michael@faide.net>
2014-10-14 19:13:51 -0700

    Factored out common styles in stylesheet

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
+(function ($) {
 
+    /*
+        Parallax effect
+        - required
+            - element start
+            - element end
+            - scroll position start
+            - scroll position end
+            - interpolation/easing function
+
+     */
+
+    var home_element       = null,
+        home_begin         = -1,
+        home_end           = -1,
+        target_element     = null,
+        target_begin       = -1,
+        target_end         = -1,
+        home_parallax      = function (element, target) {
+
+        };
+
+    $(document).ready(function () {
+        home_element = $()
+    });
+}(window.jQuery));
